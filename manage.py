f21fe2997d50759cd6b1885322102121fcef1010
Jesse Fremstad <fremstad@cs.wisc.edu>
2018-01-25 16:48:21 -0600

    Bug fixes, when called with specified parameters, will perform associated tasks, otherwise will report valid parameters.

--- manage.py
+++ manage.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 
+
 def run_tests():
     print("Running Tests")
-    return false
-    
+    return False
+
+
 def run_analysis():
     print("Analyzing Code")
-    return false
+    return False
+
 
 if __name__ == "__main__":
     import sys
 
+    keyword_function_pairs = (
+        ('test', run_tests),
+        ('analysis', run_analysis),
+    )
+
     command = None
+    error_message = ""
     if len(sys.argv) >= 2:
         command = sys.argv[1]
-    if 'test' in command:
-        run_tests()
-    elif 'analysis' in command:
-        run_analysis()
+        function_list = [func() if command == kw else None for kw, func in keyword_function_pairs]
+        if all(func is None for func in function_list):
+            error_message = "Invalid argument received"
     else:
-        return
+        error_message = "No argument specified"
+
+    if error_message:
+        parameter_list = [kw for kw, func in keyword_function_pairs]
+        print("{}, valid list is: {}".format(error_message, parameter_list))
